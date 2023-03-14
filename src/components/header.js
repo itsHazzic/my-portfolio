311cba3f6710b2b2d59ad76a262091c80ad2a306
Sarah Marr <s.bronzebeard@gmail.com>
2023-03-14 22:22:02 +0000

    added foundation code & started the home page

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return  <header className="header">
+    <Link to="/">Home</Link>
+    <Link to="contact">Contact</Link>
+    <Link to="project">Project</Link>
+</header>
+};
+
+export default Header;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return <div>
+
+        <section className="splash">
+        <img src="https://images.unsplash.com/photo-1598439210625-5067c578f3f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=60"></img>
+            <section>
+                <h1>Hello! My name is {"Sarah"}</h1>
+                <p>Title: Web Developer</p>
+            </section>
+        </section>
+    </div>
+}
+
+export default Home;
